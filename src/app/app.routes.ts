0a1a61dcb798268e43238e626fcb3e4e41afe329
RayHylian <boughelam.rachid@hotmail.fr>
2025-09-05 09:03:57 -0300

    all good with firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { PlanningComponent } from './planning/planning';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'planning' },
-  { path: 'planning', component: PlanningComponent },
-  { path: '**', redirectTo: 'planning' },
+  { path: '', component: PlanningComponent }, 
+  { path: '**', redirectTo: '' }, 
 ];
